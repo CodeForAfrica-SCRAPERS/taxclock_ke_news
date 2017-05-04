198437b44e893f3e34afeec3675dc05e3a1f9abf
chege <chegejames3@gmail.com>
2017-05-04 15:01:42 +0300

    finalilzed scraper

--- scraper.py
+++ scraper.py
@@ -3,6 +3,12 @@ import requests
 import os
 import boto3
 
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.environ['MORPH_AWS_ACCESS_KEY'],
+    aws_secret_access_key=os.environ['MORPH_AWS_SECRET_KEY'],
+    region_name='eu-west-1'
+)
 
 res = requests.get("https://www.standardmedia.co.ke/business/category/19/business-news")
 
@@ -12,19 +18,26 @@ html = BeautifulSoup(raw, 'html.parser')
 
 data = []
 
+base_url = "https://www.standardmedia.co.ke"
+
 ul = html.find("ul", class_="business-lhs")
 
 items = ul.find_all("div", class_="col-xs-6")
 
 for item in items:
     img_src = item.find("img").get("src")
+    img_url = base_url+img_src
     text = item.find("h4").text
     link = item.find("h4").find("a").get("href")
     data.append({
         'title':text,
         'link':link,
-        'img':img_src
+        'img':img_url
         })
 
-data = json.dumps(data)
-    
+s3.put_object(
+    Bucket='taxclock.codeforkenya.org',
+    ACL='public-read',
+    Key='data/standard-news.json',
+    Body=json.dumps(data)
+)
