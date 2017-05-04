42c2f4ed41e20d9ad97e9d09317c0c781fe4c4ac
chege <chegejames3@gmail.com>
2017-05-04 11:29:16 +0300

    basic scraper for standard news paper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,30 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from bs4 import BeautifulSoup
+import requests
+import os
+import boto3
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+res = requests.get("https://www.standardmedia.co.ke/business/category/19/business-news")
+
+raw = res.content
+
+html = BeautifulSoup(raw, 'html.parser')
+
+data = []
+
+ul = html.find("ul", class_="business-lhs")
+
+items = ul.find_all("div", class_="col-xs-6")
+
+for item in items:
+    img_src = item.find("img").get("src")
+    text = item.find("h4").text
+    link = item.find("h4").find("a").get("href")
+    data.append({
+        'title':text,
+        'link':link,
+        'img':img_src
+        })
+
+data = json.dumps(data)
+    
