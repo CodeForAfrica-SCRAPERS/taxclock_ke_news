61aba6b8d96f4d7d8d6ab44c66aab8c24b6756bd
Angela Mutava <angela.mutava@andela.com>
2017-08-17 20:27:04 +0300

    Added try and catch for invaid url.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import logging
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -11,36 +12,29 @@ s3 = boto3.client(
     region_name='eu-west-1'
 )
 
-res = requests.get("https://www.standardmedia.co.ke/business/category/19/business-news")
-
-raw = res.content
-
-html = BeautifulSoup(raw, 'html.parser')
-
-data = []
-
-base_url = "https://www.standardmedia.co.ke"
-
-ul = html.find("ul", class_="business-lhs")
-
-items = ul.find_all("div", class_="col-xs-6")
-
-for item in items:
-    img_src = item.find("img").get("src")
-    img_url = base_url+img_src
-    text = item.find("h4").text
-    link = item.find("h4").find("a").get("href")
+try:
+    res = requests.get("`y/19/business-news")
+    raw = res.content
+    html = BeautifulSoup(raw, 'html.parser')
+    data = []
+    base_url = "https://www.standardmedia.co.ke"
+    ul = html.find("ul", class_="business-lhs")
+    items = ul.find_all("div", class_="col-xs-6")
+    for item in items:
+        img_src = item.find("img").get("src")
+        img_url = base_url+img_src
+        text = item.find("h4").text
+        link = item.find("h4").find("a").get("href")
     data.append({
         'title':text,
         'link':link,
         'img':img_url
         })
-
-s3.put_object(
-    Bucket='taxclock.codeforkenya.org',
-    ACL='public-read',
-    Key='data/standard-news.json',
-    Body=json.dumps(data)
-)
-
-print("ok")
+    s3.put_object(
+        Bucket='taxclock.codeforkenya.org',
+        ACL='public-read',
+        Key='data/standard-news.json',
+        Body=json.dumps(data)
+        )
+except Exception as err:
+    logging.exception(str(err))
