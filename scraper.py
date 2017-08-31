945272e4cea60e65eeb2060f8b45a2707b40ead2
Angela Mutava <angela.mutava@andela.com>
2017-08-31 11:10:33 +0300

    edit standard media scrape method for uniformity.

--- scraper.py
+++ scraper.py
@@ -1,27 +1,20 @@
-from scrapers.config import scrape_sites, base_urls
-from scrapers.base import Scraper
 from scrapers.capital import CapitalMedia
 from scrapers.the_star import StarMedia
 from scrapers.nation import NationMedia
+from scrapers.standard import StandardMedia
 
-'''
- This file contains the instances\
-  of the scraper classes
- It's the entry point\
- to all scraping.
-'''
 
 '''
 Instances of all the classes.
 '''
 
-scraper = Scraper()
+standard = StandardMedia()
 capital = CapitalMedia()
 star = StarMedia()
 nation = NationMedia()
 
 # scraps for the standard media site
-scraper.scrape(scrape_sites["standard"], base_urls["standard"])
+standard.scrape_page()
 
 # Scrapes the capitalfm site
 capital.scrape_page()
